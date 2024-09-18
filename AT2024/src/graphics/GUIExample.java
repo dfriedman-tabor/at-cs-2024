c6a5bc6ae37dac2b08421f98706c2ece369b8dbd
dfriedman <dfriedman@172.16.8.247>
2024-09-18 12:03:15 -0400

    comments added

--- AT2024/src/graphics/GUIExample.java
+++ AT2024/src/graphics/GUIExample.java
@@ -1,4 +1,4 @@
-package arrays2d;
+package graphics;
 
 import java.awt.Color;
 import java.awt.Dimension;
@@ -13,25 +13,32 @@ import javax.swing.JPanel;
 
 public class GUIExample {
 	
+	// constants (to avoid magic numbers)
 	final int WIDTH = 800, HEIGHT = 600, CANVAS_HEIGHT = HEIGHT * 2 / 3;
 
 	public GUIExample() {
 		
 		JFrame window = new JFrame("GUI Example");
 		window.setSize(WIDTH, HEIGHT);
-		window.setLocationRelativeTo(null);
+		window.setLocationRelativeTo(null);	// centers the window 
 		
+		// will hold everything - given a vertical format
 		JPanel mainContainer = new JPanel();
 		mainContainer.setLayout(new BoxLayout(mainContainer, BoxLayout.Y_AXIS));
 		
+		// holds all buttons 
 		JPanel buttonContainer = new JPanel();
 		buttonContainer.setPreferredSize(new Dimension(WIDTH, HEIGHT - CANVAS_HEIGHT));
 		buttonContainer.setLayout(new BoxLayout(buttonContainer, BoxLayout.Y_AXIS));
 
+		// a container for each row of buttons
+		// no formatting bc horizontal alignment is default
 		JPanel row1 = new JPanel();
 		JPanel row2 = new JPanel();
 		JPanel row3 = new JPanel();
 		
+		// create a button titled 'circle'
+		// give it a listener that reacts when button is clicked
 		JButton circleButton = new JButton("Circle");
 		circleButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -49,9 +56,12 @@ public class GUIExample {
 		buttonContainer.add(row3);
 		
 		
+		// create our canvas, and define how it should appear 
 		JPanel canvas = new JPanel() {
 			public void paint(Graphics g) {
-				g.setColor(Color.white);
+				
+				// draws a white rectangle the size of the canvas
+				g.setColor(Color.white);		
 				g.fillRect(0, 0, GUIExample.this.WIDTH, CANVAS_HEIGHT);
 			}
 		};
@@ -64,7 +74,10 @@ public class GUIExample {
 		window.add(mainContainer);
 		
 		
+		// terminate the program when the window is closed
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// make the window and all inner components visible
 		window.setVisible(true);
 	}
 	
